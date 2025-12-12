5b4df7770df72988a17f508200f470cd9a4df4f0
aboofainaz <aboofainaz@gmail.com>
2025-12-12 20:36:22 +0530

    feat: initialize Django project settings with base configuration, installed apps, middleware, and environment variable handling.

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if 'VERCEL' in os.environ:
+    MEDIA_ROOT = Path('/tmp/media')
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
